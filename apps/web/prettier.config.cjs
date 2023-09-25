9b17c606b99735feff08b66b46e52a5cb8a6851f
Miljan <miljancode@gmail.com>
2023-09-25 21:51:30 +0200

    init: web app

---
+++ apps/web/prettier.config.cjs
@@ -0,0 +1,27 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+
+// @ts-expect-error SortImportsConfig missing
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrderTypeScriptVersion: "5.0.0",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/(.*)$",
+  ],
+  proseWrap: "always",
+};
+
+module.exports = config;
