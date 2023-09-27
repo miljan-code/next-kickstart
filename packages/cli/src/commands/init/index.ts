ccc09f05e8603359e972ee05c0663d103d00e009
Miljan <miljancode@gmail.com>
2023-09-27 15:35:22 +0200

    fix: init project without init cmd

--- packages/cli/src/commands/init/index.ts
+++ packages/cli/src/commands/init/index.ts
@@ -14,7 +14,7 @@ import { installDeps } from "./helpers/install-deps.js";
 import { generateKickstartConfig } from "./helpers/generate-kickstart-config.js";
 import { logger } from "@/utils/logger.js";
 import { renderTitle } from "@/utils/render-title.js";
-import { DEFAULT_APP_NAME } from "@/constants.js";
+import { printNextSteps } from "./helpers/print-next-steps.js";
 
 const ALL_PACKAGES = {
   drizzle: true,
@@ -29,41 +29,44 @@ const initOptionsSchema = z.object({
 
 const initDirSchema = z.string().min(1);
 
+export const initAction = async (dir: string | undefined, opts: string) => {
+  renderTitle();
+
+  const { projectName, dirName } = await getProjectName(dir);
+  const { yes: fullInstall } = initOptionsSchema.parse(opts);
+  const initDir = initDirSchema.parse(dirName);
+  const projectDir = parsePath(initDir);
+
+  const packages = fullInstall ? ALL_PACKAGES : await checkPackages();
+  const installs = await checkInstalls();
+
+  const initGit = installs.git;
+  const shouldInstallDeps = installs.deps;
+
+  // Generate starter next app
+  await generateStarter({ projectDir, projectName, initGit });
+
+  // Add packages
+  installPackages({ projectDir, packages });
+
+  // Create ENV files
+  createEnv({ projectDir, packages });
+
+  // Generate config JSON file
+  generateKickstartConfig({ projectDir, packages });
+
+  // Install deps
+  if (shouldInstallDeps) await installDeps(projectDir);
+
+  logger.info("Project has been successfully initialized");
+  printNextSteps({ packages, projectName, shouldInstallDeps });
+  logger.success("\nHappy hacking!\n");
+  process.exit(0);
+};
+
 export const init = new Command()
   .name("init")
   .description("initialize new project")
   .argument("[dir]", "directory to init a project")
   .option("-y, --yes", "skip confirmation prompt", false)
-  .action(async (dir: string | undefined, opts: string) => {
-    renderTitle();
-
-    const { projectName, dirName } = await getProjectName(dir);
-    const { yes: fullInstall } = initOptionsSchema.parse(opts);
-    const initDir = initDirSchema.parse(dirName);
-    const projectDir = parsePath(initDir);
-
-    const packages = fullInstall ? ALL_PACKAGES : await checkPackages();
-    const installs = await checkInstalls();
-
-    const initGit = installs.git;
-    const shouldInstallDeps = installs.deps;
-
-    // Generate starter next app
-    await generateStarter({ projectDir, projectName, initGit });
-
-    // Add packages
-    installPackages({ projectDir, packages });
-
-    // Create ENV files
-    createEnv({ projectDir, packages });
-
-    // Generate config JSON file
-    generateKickstartConfig({ projectDir, packages });
-
-    // Install deps
-    if (shouldInstallDeps) await installDeps(projectDir);
-
-    logger.info("Project has been successfully initialized");
-    logger.success("\nHappy hacking!\n");
-    process.exit(0);
-  });
+  .action(initAction);
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,7 +2,7 @@
 
 import { Command } from "commander";
 import { getPackageInfo } from "./utils/get-package-info.js";
-import { init } from "./commands/init/index.js";
+import { init, initAction } from "./commands/init/index.js";
 
 function main() {
   const packageInfo = getPackageInfo();
@@ -14,7 +14,10 @@ function main() {
       packageInfo.version || "0.1.0",
       "-v, --version",
       "display the version number",
-    );
+    )
+    .argument("[dir]", "directory to init a project")
+    .option("-y, --yes", "skip confirmation prompt", false)
+    .action(initAction);
 
   program.addCommand(init);
 
