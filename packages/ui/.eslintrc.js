b694132df31b79a7e59a549413d7b8e519a80d98
Miljan <miljancode@gmail.com>
2023-10-04 12:30:36 +0200

    feat: added husky pre commit

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,6 +1,6 @@
-const { resolve } = require('node:path');
+const { resolve } = require("node:path");
 
-const project = resolve(process.cwd(), 'tsconfig.json');
+const project = resolve(process.cwd(), "tsconfig.json");
 
 /*
  * This is a custom ESLint configuration for use with
@@ -14,9 +14,9 @@ const project = resolve(process.cwd(), 'tsconfig.json');
 
 module.exports = {
   extends: [
-    '@vercel/style-guide/eslint/browser',
-    '@vercel/style-guide/eslint/typescript',
-    '@vercel/style-guide/eslint/react',
+    "@vercel/style-guide/eslint/browser",
+    "@vercel/style-guide/eslint/typescript",
+    "@vercel/style-guide/eslint/react",
   ].map(require.resolve),
   parserOptions: {
     project,
@@ -25,13 +25,13 @@ module.exports = {
     JSX: true,
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       typescript: {
         project,
       },
     },
   },
-  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
 
   rules: {
     // add specific rules configurations here
