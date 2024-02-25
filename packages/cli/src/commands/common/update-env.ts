36c599575d2a5a948a7ff095486c7af39f5bb63a
Miljan <miljancode@gmail.com>
2024-02-25 14:35:01 +0100

    :recycle: refactored env vars generation logic

--- packages/cli/src/commands/common/update-env.ts
+++ packages/cli/src/commands/common/update-env.ts
@@ -4,6 +4,7 @@ import fs from "fs-extra";
 import { PKG_ROOT } from "@/constants.js";
 import { type InstallPackagesOpts } from "../init/helpers/install-packages.js";
 import { type AvailablePackage } from "@/commands/add/helpers/check-argument.js";
+import { type Packages } from "./prompts.js";
 
 const envVariables = {
   drizzle: {
@@ -24,32 +25,40 @@ const envVariables = {
 export const createEnv = ({ packages, projectDir }: InstallPackagesOpts) => {
   let envContent = "";
 
-  for (const pkg of Object.keys(envVariables)) {
-    if (!packages[pkg as keyof typeof packages]) continue;
-    for (const [key, value] of Object.entries(
-      envVariables[pkg as keyof typeof envVariables],
-    )) {
+  for (const pkg of getKeys(envVariables)) {
+    if (!packages[pkg]) continue;
+    for (const [key, value] of Object.entries(envVariables[pkg])) {
       envContent += `${key}=${value}`;
     }
   }
 
   const envPath = path.join(projectDir, ".env.example");
-  fs.writeFileSync(envPath, envContent);
-
-  // TODO: add UT to t3ENV
-
-  const t3EnvFile =
-    packages.drizzle && packages.nextauth
-      ? "env-auth-db.mjs"
-      : packages.drizzle
-      ? "env-db.mjs"
-      : packages.nextauth
-      ? "env-auth.mjs"
-      : "env-base.mjs";
-  const t3EnvSrc = path.join(PKG_ROOT, "template/libs/providers", t3EnvFile);
+  const t3EnvSrc = path.join(PKG_ROOT, "template/libs/providers/env.mjs");
   const t3EnvDest = path.join(projectDir, "env.mjs");
 
+  fs.writeFileSync(envPath, envContent);
   fs.copySync(t3EnvSrc, t3EnvDest);
+  removeUselessVars(t3EnvDest, packages);
 };
 
 export const updateEnv = (projectDir: string, pkg: AvailablePackage) => {};
+
+function removeUselessVars(filePath: string, packages: Packages) {
+  let content = fs.readFileSync(filePath, "utf-8");
+
+  for (const pkg of getKeys(envVariables)) {
+    if (packages[pkg]) continue;
+    for (const envVar of getKeys(envVariables[pkg])) {
+      content = content
+        .split("\n")
+        .filter((line) => !line.includes(envVar))
+        .join("\n");
+    }
+  }
+
+  fs.writeFileSync(filePath, content, "utf-8");
+}
+
+function getKeys<T extends {}>(obj: T) {
+  return Object.keys(obj) as Array<keyof T>;
+}
